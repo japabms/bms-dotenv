7f4e76edcabfcccf8e39fcf1db79b30a47f2b695
Victor Matheus <matheuscorreaosu@gmail.com>
2024-02-28 20:55:32 -0300

    docs: docs.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# .env parser
+# C .env parser  
+
+- Will find a .env file and will load the values from it. The values must me KEY=VALUE, without any space. And it will not remove quotes from VALUES.
+- Support comment with #.
+- It will probably only work in Unix systems.
+
+I made it in like 3 hours, so it's not refined yet. But i think it is usable.
+
+## Build / Install
+
+To build:
+```
+make build
+```
+
+To install:
+```
+sudo make install
+```
+If you install it, you can link it with the flag -lbmsdotenv.
--- example.c
+++ example.c
@@ -1,11 +1,10 @@
-#include "bms_dotenv.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <bms_dotenv.h>
 
 int main(int argc, char *argv[])
 {
-
 
 	if(bms_dotenv_init(NULL) < 0) {
 		return -1;
@@ -13,29 +12,23 @@ int main(int argc, char *argv[])
 
 	char* api_key = bms_dotenv_get("API_KEY");
 	if(api_key == NULL) {
-		// do something
+		// cannot retrieve the value
 	}
 	char* api_url = bms_dotenv_get("API_URL");
 	if(api_url == NULL) {
-		// do something
+		// cannot retrieve the value
 	}
 	
 
-	puts(api_key);
-	puts(api_url);
 
 	char* host = bms_dotenv_get("HOST");
 	if(host == NULL) {
-		// do something
+		// cannot retrieve the value
 	}
 	char* port = bms_dotenv_get("PORT");
 	if(port == NULL) {
-		// do something
+		// cannot retrieve the value
 	}
-
-	puts(host);
-	puts(port);
-
 
 	bms_dotenv_finalize();
 
